f23ce57e442345c8419d43975bf39c46756e5c92
luxun <401871194@qq.com>
2021-08-29 13:10:07 +0800

    测试ci

---
+++ utils/hello/index.js
@@ -0,0 +1,22 @@
+/*
+ * @loadImg:   
+ * @title:    load image
+ * @title-zh: 加载图片 
+ * @desc:     this is description
+ * @desc-zh:  描述描述
+ * @tags:     img\file
+ * @usage:    这是用法
+ */
+
+function hello(url){
+    return new Promise((resolve,reject) => {
+        const img = new Image();
+        img.src = url;
+        img.onload = () => {
+            resolve(img);
+        }
+        img.onerror = () => {
+            reject();
+        }
+    })
+}
