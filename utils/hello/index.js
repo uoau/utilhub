9dc6c8d6848a3fb83f295dda168a230c0ce7cfdf
luxun <401871194@qq.com>
2021-08-29 16:50:11 +0800

    zzz9

--- utils/hello/index.js
+++ utils/hello/index.js
@@ -9,7 +9,7 @@
  */
 
 function hello(url){
-    console.log(2121244422233333311);
+    console.log('我是天才');
     return new Promise((resolve,reject) => {
         const img = new Image();
         img.src = url;
