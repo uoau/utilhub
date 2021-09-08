76271ac8be996e44f77cbb127e370331376154d9
luxun <401871194@qq.com>
2021-09-08 23:22:00 +0800

    测试

--- utils/sleep/info.js
+++ utils/sleep/info.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'sleep',
     title: 'sleep一段时间22222333334444新新新',
     titleZh: 'promise 睡眠一段时间',
-    desc: '',
-    descZh: '',
+    desc: 'hello world!',
+    descZh: '更新客户端版本。当判断用户小程序所在客户端版本过低时，可使用该接口跳转到更新微信页面。',
     tags: ['progress'],
 };
